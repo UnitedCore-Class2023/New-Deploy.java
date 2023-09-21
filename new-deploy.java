8e663cf2944256bd1374ceffb5d17b9917aa3a36
Isaac Ntim <unitedcoresystems@gmail.com>
2023-09-21 01:22:28 +0000

    my 2nd try commit

---
+++ new-deploy.java
@@ -0,0 +1,29 @@
+Deploy.java
+    echo "Welcome to Zelle"                    = Isaac
+    echo "Zelle your payment gateway"          = Ben        
+    echo "Zelle is the gateway of the future"  = UnitedCore   
+    echo "Zelle is offering 30% discount"      = Emma
+    echo "Good deals for returning clients"     = Theo
+
+Repository:
+    https://github.com/UnitedCore-Class2023/Deploy.java.git
+
+creat a directory
+git init 
+git status
+git add . 
+git commit -m "comments"
+git remote add origin https://github.com/UnitedCore-Class2023/Deploy.java.git
+
+
+Git Branch :
+
+  Master
+  stage branches
+  development
+  main
+
+
+ssh-keygen -t ed25519 -C "your_email@example.com"
+
+  ssh-keygen -t ed25519 -C "unitedcoresystems@gmail.com"
